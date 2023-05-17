aa84dcedbcc4840754fb516cde63c4ee9b1439b3
Dmitrijevv <dmitrijevv@icloud.com>
2023-05-17 07:25:21 +0200

    api upd

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import * as axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5050/api/',
-    //  baseURL: 'https://server-m2.vercel.app/api/',
+    // baseURL: 'http://localhost:5050/api/',
+    baseURL: 'https://server-m2.vercel.app/api/',
     timeout: 2000,
     headers: {
         'Access-Control-Allow-Origin': '*',
